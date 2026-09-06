391f49c22ce2839558fd0d934d28d419b0aa888c
Muhammadjon <kamronmirqosimov20100@gmail.com>
2026-09-06 10:43:50 +0500

    Add explicit CORS methods and headers

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ app.use(
          }
       },
       credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization']
    })
 );
 app.use(express.json());
