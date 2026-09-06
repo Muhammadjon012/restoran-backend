99931db2cd6a683e53ab69b98eb28bf93aa307c1
Muhammadjon <kamronmirqosimov20100@gmail.com>
2026-09-06 10:41:14 +0500

    Add CORS logging for debugging

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ connectDB().then(() => ensureDefaultAdmin());
 const frontendUrl = process.env.FRONTEND_URL || 'https://restoran-frontendd.onrender.com';
 const allowedOrigins = frontendUrl.split(',').map(url => url.trim());
 
+console.log('Allowed CORS origins:', allowedOrigins);
+
 app.use(
    cors({
       origin: function (origin, callback) {
@@ -46,6 +48,7 @@ app.use(
             callback(null, true);
          } else {
             console.log('CORS blocked for origin:', origin);
+            console.log('Allowed origins are:', allowedOrigins);
             callback(new Error('Not allowed by CORS'));
          }
       },
